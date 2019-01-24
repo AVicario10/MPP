0e009aebf9586ae07410a7aaaa31fbe1dda940fc
AVicario10 <andertxu.2003@gmail.com>
2019-01-24 20:20:21 +0100

    Añadiendo del archivo hello

--- pokemon_battle.py
+++ pokemon_battle.py
@@ -1,4 +1,4 @@
-enemy = input('Choose the pokemon you want to battle against! (Charmander) / (Bulbasaur) / (Squirtle)\n')
+enemy = input('Aukera ezazu zein Pokemonen aurka borrokatu nahi duzun! (Charmander) / (Bulbasaur) / (Squirtle)\n')
 
 pikachu_hp = 35
 pikachu_attack = 55
@@ -16,87 +16,111 @@ damage = 0
 if enemy == "Charmander":
     enemy_hp = 39
     enemy_attack = 52
-    enemy_attack_movement = "Scratch"
+    enemy_attack_movement = "Urrakada"
     enemy_attack_movement_damage = 21
     enemy_defense = 43
     enemy_speed = 65
 elif enemy == "Bulbasaur":
     enemy_hp = 45
     enemy_attack = 49
-    enemy_attack_movement = "Tackle"
+    enemy_attack_movement = "Plakaketa"
     enemy_attack_movement_damage = 20
     enemy_defense = 65
     enemy_speed = 45
 elif enemy == "Squirtle":
     enemy_hp = 44
     enemy_attack = 48
-    enemy_attack_movement = "Tackle"
+    enemy_attack_movement = "Plakaketa"
     enemy_attack_movement_damage = 19
     enemy_defense = 49
     enemy_speed = 43
 
 while pikachu_hp > 0 or enemy_hp > 0:
-    chosen_atack = input("Choose the movement: (Quick attack / Thunder shock)\n")
+    chosen_attack = input("Mugimendua aukera ezazu: (Eraso azkarra / Trumoi kolpea)\n")
 
-    if chosen_atack == "Quick attack":
+    if chosen_attack == "Eraso azkarra":
         chosen_attack_damage = 22
         if pikachu_speed > enemy_speed:
             damage = int(chosen_attack_damage * enemy_defense / (200 - pikachu_attack))
             enemy_hp -= damage
             if damage > enemy_hp:
                 enemy_hp = 0
-            print("Pikachu used Quick attack!")
-            print("Enemy {} has {} hp points!".format(enemy, enemy_hp))
+            print("Pikachuk Eraso azkarra erabili du!")
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} hp puntu ditu!".format(enemy, enemy_hp))
+            else:
+                print("Arerioa, {}k {} hp puntu ditu!".format(enemy, enemy_hp))
             if enemy_hp <= 0:
-                print("Enemy {} fainted".format(enemy))
-                print("The battle has finished.")
+                print("Arerioa, {} akabatuta.".format(enemy))
+                print("Borroka bukatu da.")
                 break
             pikachu_hp -= int(enemy_attack_movement_damage * pikachu_defense / (200 - enemy_attack))
-            print("Enemy {} used {}!".format(enemy, enemy_attack_movement))
-            print("Pikachu has {} hp points!\n".format(pikachu_hp))
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} erabili du!".format(enemy, enemy_attack_movement))
+            else:
+                print("Arerioa, {}k {} erabili du!".format(enemy, enemy_attack_movement))
+            print("Pikachuk {} hp puntu ditu!\n".format(pikachu_hp))
         else:
             pikachu_hp -= int(enemy_attack_movement_damage * pikachu_defense / (200 - enemy_attack))
             if enemy_attack_movement_damage > pikachu_hp:
                 pikachu_hp = 0
-            print("Enemy {} used {}!".format(enemy, enemy_attack_movement))
-            print("Pikachu has {} hp points!".format(pikachu_hp))
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} erabili du!".format(enemy, enemy_attack_movement))
+            else:
+                print("Arerioa, {}k {} erabili du!".format(enemy, enemy_attack_movement))
+            print("Pikachuk {} hp puntu ditu!".format(pikachu_hp))
             if enemy_hp <= 0:
-                print("Enemy {} fainted".format(enemy))
-                print("The battle has finished.")
+                print("Arerioa, {} akabatuta.".format(enemy))
+                print("Borroka bukatu da.")
                 break
             damage = int(chosen_attack_damage * enemy_defense / (200 - pikachu_attack))
             enemy_hp -= damage
-            print("Pikachu used Quick attack!")
-            print("Enemy {} has {} hp points!\n".format(enemy, enemy_hp))
+            print("Pikachu Eraso azkarra erabili du!")
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} hp puntu ditu!\n".format(enemy, enemy_hp))
+            else:
+                print("Arerioa, {}k {} hp puntu ditu!\n".format(enemy, enemy_hp))
 
-    elif chosen_atack == "Thunder shock":
+    elif chosen_attack == "Trumoi kolpea":
         chosen_attack_damage = 27
         if pikachu_speed > enemy_speed:
             damage = int(chosen_attack_damage * enemy_defense / (200 - pikachu_attack))
             enemy_hp -= damage
             if damage > enemy_hp:
                     enemy_hp = 0
-            print("Pikachu used Thunder shock!")
-            print("Enemy {} has {} hp points!".format(enemy, enemy_hp))
+            print("Pikachu Trumoi kolpea erabili du!")
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} hp puntu ditu!".format(enemy, enemy_hp))
+            else:
+                print("Arerioa, {}k {} hp puntu ditu!".format(enemy, enemy_hp))
             if enemy_hp <= 0:
-                print("Enemy {} fainted".format(enemy))
-                print("The battle has finished.")
+                print("Arerioa {} akabatuta.".format(enemy))
+                print("Borroka bukatu da.")
                 break
             pikachu_hp -= int(enemy_attack_movement_damage * pikachu_defense / (200 - enemy_attack))
-            print("Enemy {} used {}!".format(enemy, enemy_attack_movement))
-            print("Pikachu has {} hp points!\n".format(pikachu_hp))
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa {}rek {} erabili du!".format(enemy, enemy_attack_movement))
+            else:
+                print("Arerioa {}k {} erabili du!".format(enemy, enemy_attack_movement))
+            print("Pikachuk {} hp puntu ditu!\n".format(pikachu_hp))
         else:
             pikachu_hp -= int(enemy_attack_movement_damage * pikachu_defense / (200 - enemy_attack))
             if enemy_attack_movement_damage > pikachu_hp:
                 pikachu_hp = 0
-            print("Enemy {} used {}!".format(enemy, enemy_attack_movement))
-            print("Pikachu has {} hp points!".format(pikachu_hp))
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} erabili du!".format(enemy, enemy_attack_movement))
+            else:
+                print("Arerioa, {}k {} erabili du!".format(enemy, enemy_attack_movement))
+            print("Pikachuk {} hp puntu ditu!".format(pikachu_hp))
             if enemy_hp <= 0:
-                print("Enemy {} fainted".format(enemy))
-                print("The battle has finished.")
+                print("Arerioa, {} akabatuta.".format(enemy))
+                print("Borroka bukatu da.")
                 break
             damage = int(chosen_attack_damage * enemy_defense / 145)
             enemy_hp -= damage
-            print("Pikachu used Thunder shock!")
-            print("Enemy {} has {} hp points!\n".format(enemy, enemy_hp))
+            print("Pikachu Trumoi kolpea erbili du!")
+            if enemy == "Charmander" or "Bulbasaur":
+                print("Arerioa, {}rek {} hp puntu ditu!\n".format(enemy, enemy_hp))
+            else:
+                print("Arerioa, {}k {} hp puntu ditu!\n".format(enemy, enemy_hp))
 
